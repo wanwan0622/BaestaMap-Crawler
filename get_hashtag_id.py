32e746111d4cbf49b0db721d042b1217b296bdb2
xryuseix <ryusei143.shootingstar@gmail.com>
2022-08-01 10:48:41 +0900

    refactor

--- get_hashtag_id.py
+++ get_hashtag_id.py
@@ -2,16 +2,15 @@ import requests
 import json
 
 
-
-
-def get_hash_id(query, credentials):
+def get_hashtag_id(query, credentials) -> str:
     INSTAGRAM_ID = credentials["instagram_id"]
     ACCESS_TOKEN = credentials["access_token"]
-    
+
     id_search_url = f"https://graph.facebook.com/ig_hashtag_search?user_id={INSTAGRAM_ID}&q={query}&access_token={ACCESS_TOKEN}"
     response = requests.get(id_search_url)
-    hash_id = response.json()["data"][0]["id"]
-    return hash_id
+    hashtag_id = response.json()["data"][0]["id"]
+    return hashtag_id
+
 
 if __name__ == "__main__":
     with open("credencials.json") as f:
@@ -19,5 +18,6 @@ if __name__ == "__main__":
 
     # 検索したいワード
     query = "新宿ランチ"
-    hash_id = get_hash_id(query, credentials)
-    print(hash_id)
+    hashtag_id = get_hashtag_id(query, credentials)
+    print(hashtag_id)
+ 
