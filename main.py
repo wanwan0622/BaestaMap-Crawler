032ce93b88c674259d9f9fec0d96545ba5a1e211
xryuseix <ryusei143.shootingstar@gmail.com>
2022-09-03 05:36:53 +0900

    feat: 実行する関数をmodeで切り替えられるようにした

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from Firestore import FireStore
 from instagrapi import Client
 from credentials import getCredentials
+from postLocation import post_location
 from get_hashtag_id import addHashTagId
 from get_posts import getPosts
 
 print("info: initialize apps")
 fs = FireStore()
 credentials = getCredentials()
-cl = Client()
-cl.login(credentials["instagram_screen_name"], credentials["instagram_pw"])
 
 print("info: server start!")
 
-getPosts(fs, cl, credentials)
-# addHashTagId(fs, credentials)
+mode = 2
+
+if mode == 1:
+    post_location(fs)
+elif mode == 2:
+    addHashTagId(fs, credentials)
+elif mode == 3:
+    cl = Client()
+    cl.login(credentials["instagram_screen_name"], credentials["instagram_pw"])
+    getPosts(fs, cl, credentials)
